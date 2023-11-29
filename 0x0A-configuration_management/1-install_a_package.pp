46f0d01dffb233890f46bc6870eff3b53a893750
gladysdo <doe.glad@gmail.com>
2023-11-29 03:37:37 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # Install Werkzeug first
 package { 'werkzeug':
-  ensure   => '2.3.7',
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
 
 # Install Flask with a specific version
 package { 'flask':
-  ensure   => '2.2.2',
+  ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['werkzeug'],
 }
